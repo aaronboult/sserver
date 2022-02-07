90d1887f5b5d74876d256d9559ab73a1969397ab
Aaron Boult <aboult@pmdcreate.com>
2022-02-07 11:22:23 +0000

    Added route manifest & clearing

--- sserver/tool/RouteTools.py
+++ sserver/tool/RouteTools.py
@@ -14,7 +14,10 @@ class RouteTools:
     @staticmethod
     def clear():
         Logger.log('Clearing routes')
-        # @todo Implement route clearing
+        route_manifest = CacheTools.pop('route_manifest', default = [])
+        CacheTools.delete_bulk(route_manifest)
+
+
 
 
     #
@@ -27,10 +30,18 @@ class RouteTools:
 
         route_module_list = ModuleTools.load_from_filename('routes.py', fromlist=['routes'])
 
+        route_manifest = []
+
         Logger.log('Loading Routes...')
         for module in route_module_list:
             route_list = ModuleTools.get_from_module(module, 'routes', [])
 
             for route in route_list:
                 Logger.log('Found Route {}, handled by {}'.format(route.url, str(route.endpoint)))
+
+                # Assign route and add to manifest
                 CacheTools.serialize_set(route.url, route)
+
+                route_manifest.append(route.url)
+
+        CacheTools.serialize_set('route_manifest', route_manifest)
