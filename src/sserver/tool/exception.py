717a363ee7001e3a24b178a8d55f6463b3383194
Aaron <aboult@pmdcreate.com>
2022-04-24 23:23:10 +0100

    Missed in previous commit

---
+++ src/sserver/tool/exception.py
@@ -0,0 +1,12 @@
+#
+# Cache Not Initialized Exception
+#
+class CacheNotInitializedException(Exception):
+    pass
+
+
+#
+# Cache Already Initialized Exception
+#
+class CacheAlreadyInitializedException(Exception):
+    pass
