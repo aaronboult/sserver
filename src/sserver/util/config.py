dfa1dbd11805616e981c413736d4bb5e8b3e8494
Aaron Boult <aaronboult@gmail.com>
2022-09-04 20:35:33 +0100

    Changed default template folder name

--- src/sserver/util/config.py
+++ src/sserver/util/config.py
@@ -34,7 +34,7 @@ __PROJECT_DEFAULT_CONFIG = {
 
 __APP_DEFAULT_CONFIG = {
     'prefix_route_with_app_name': True,
-    'template_folder': 'templates',
+    'template_folder': 'template',
     'static_image_folder': 'static/image',
     'static_css_folder': 'static/css',
     'static_js_folder': 'static/js',
