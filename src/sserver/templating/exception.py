872bf1c3a5cbb780469d391ae442ad925a22505c
Aaron Boult <aaronboult25@gmail.com>
2022-09-27 00:18:58 +0100

    Missing tag function exception

--- src/sserver/templating/exception.py
+++ src/sserver/templating/exception.py
@@ -9,8 +9,14 @@ class MissingTagArgumentsException(Exception):
 class UnknownTagException(Exception):
     """Raised when a template tag is called with an unknown tag name."""
 
+
 class UnclosedBlockTagException(Exception):
     """Raised when a block tag is not closed."""
 
+
 class UnknownTagConditionalExpressionException(Exception):
     """Raised when a conditional expression is not recognized."""
+
+
+class MissingTagFunctionException(Exception):
+    """Raised when a tag function is not found."""
